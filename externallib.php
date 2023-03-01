61be36fd1c3a159b6d0ce73b11fcb0890576df73
tecmd-dev <123190831+tecmd-dev@users.noreply.github.com>
2023-03-01 10:35:41 -0500

    Ajuste función
    Se actualiza la función, el método propio de moodle trae el porcentaje de avance a si las unidades o actividades estén activas.

--- externallib.php
+++ externallib.php
@@ -6,20 +6,13 @@
  * and open the template in the editor.
  */
 
-require_once '../../config.php';
-require_once($CFG->dirroot . '/lib/externallib.php');
+ require_once '../../config.php';
+ require_once($CFG->dirroot. '/lib/externallib.php');
 
 use core_completion\progress;
 
 class local_uniminuto_external_api extends external_api
 {
-
-    /**
-     * Declara y retorna los parámetros a ser usados en la función del web services.
-     *
-     * @return array Devuelve un array con todos los parámetros que harán parte
-     * de nuestro web services.
-     */
 
     public static function uniminuto_get_course_percentage_parameters()
     {
@@ -31,58 +24,69 @@ class local_uniminuto_external_api extends external_api
         );
     }
 
-    /**
-     * Función que retorna el porcentaje de avance en un curso con un usuario y un curso especifico 
-     * 
-     * @param int $course_id id del curso a consultar
-     * @param int $user_id id del usuario a consultar
-     * 
-     * @return object objeto que contiene el id del curso, el id del usuario y el porcentaje de
-     * avance del curso.
-     */
-
-    public static function uniminuto_get_course_percentage($course_id, $user_id)
+    public function uniminuto_get_course_percentage($course_id,$user_id)
     {
+
         global $DB;
-
-        $dataCourse = $DB->get_record('course', array('id' => $course_id));
-
-        if ($dataCourse) {
-
-            $percentage = progress::get_course_progress_percentage($dataCourse, $user_id);
-            $percentageFinish = ($percentage === null) ? "No data" : $percentage;
-            $resp = new stdClass();
-            $resp->course_id = $course_id;
-            $resp->user_id = $user_id;
-            $resp->percentage = $percentageFinish;
-            return $resp;
-
-        } else {
-
-            $resp = new stdClass();
-            $resp->course_id = "No data";
-            $resp->user_id = "No data";
-            $resp->percentage = "No data";
-            return $resp;
+        $mycourses = enrol_get_users_courses($user_id, true);
+        $check_course = false;
+        $percentageFinish = "No data";
+        foreach ($mycourses as $mycourse) {
+            if( $course_id == $mycourse->id ){ $check_course = true; break; }
         }
+
+        if( $check_course )
+        {
+            $dataCourse = $DB->get_record('course', array('id' => $course_id));
+            $modinfo = get_fast_modinfo($course_id, $user_id)->get_cms();//Obtener info del todo el curso
+            $completioninfo = new \completion_info($dataCourse);
+            $activities_complete = $activities_completion = $activity_completed = 0;
+
+            foreach ($modinfo as $cm) {
+                $infosection = $cm->get_section_info(); //Informacion de la seccion
+                $hiddensection = ($infosection->available && ($infosection->uservisible || $infosection->visible)) ? false : true ;
+                if ( $hiddensection == false ){
+                    //Si la actividad se encuentra visible
+                    if ($cm->modname == 'label') { continue; }
+                    if ( $cm->deletioninprogress == 0 ){
+                        if ( $cm->visible || ( $cm->uservisible && $cm->available && $cm->visibleoncoursepage && !$cm->is_stealth()) ) {
+                            $activity_completed = $completioninfo->get_data($cm, true, $user_id)->completionstate;
+                            $activities_complete += $activity_completed;
+                            if ( $cm->completion > 0 ) { $activities_completion++; }
+                        }
+                    }
+                }
+            }
+
+            if ($activities_completion != 0)
+            {
+                $percent_complete = $activities_complete * 100 / $activities_completion;
+                $percentageFinish = intval($percent_complete);
+            }
+        }
+
+
+        $resp = new stdClass();
+        $resp->course_id = $course_id;
+        $resp->user_id = $user_id;
+        $resp->percentage = $percentageFinish;
+        return $resp;
+                   
     }
 
-
-    /**
-     * Función que retorna la respuesta del web services 
-     * 
-     * @return array retorna la respuesta como un array con cada uno de los valores
-     * especificados en la función anterior uniminuto_get_course_percentage
+    /* 
+     * Notificar al nodo de una actualización
+     * Método para estructurar la respuesta
      */
-
     public static function uniminuto_get_course_percentage_returns()
     {
         return new external_single_structure(
             array(
                 'course_id'  => new external_value(PARAM_RAW, ''),
                 'user_id'  => new external_value(PARAM_RAW, ''),
-                'percentage'  => new external_value(PARAM_RAW, ''),
+                'percentage'  => new external_value(PARAM_RAW, '')
             )
         );
     }
+
 }
